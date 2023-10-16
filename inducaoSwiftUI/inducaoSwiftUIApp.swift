4efa0022f71fca3aa6806bac41b15b7870e5251b
Marcus Vinícius Tibúrcio <127891536+macustiburcio23@users.noreply.github.com>
2023-10-15 22:29:27 -0300

    Initial commit

---
+++ inducaoSwiftUI/inducaoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  inducaoSwiftUIApp.swift
+//  inducaoSwiftUI
+//
+//  Created by Marcus Vinicus Tiburcio on 15/10/23.
+//
+
+import SwiftUI
+
+@main
+struct inducaoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
